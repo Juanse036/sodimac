baccb1f5880bfc6b220b0b0eb914ec2ff6aee88e
juanse036 <juansebas036@gmail.com>
2026-04-11 21:29:18 -0500

    Agregar Mas rutas

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home'
 import { ProductDetail } from './pages/ProductDetail';
+import { Cart } from './pages/Cart';
+import { NotFound } from './pages/NotFound';
 import { Navbar } from './components/Navbar';
-
+import { CartProvider } from './context/CartContext';
 
 function App() {
 
   return (
     <Router>
+      <CartProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
+    </CartProvider>
     </Router>
   );
 }
